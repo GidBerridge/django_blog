283892ef273c39272f9d2bf5ca882581adbd2c5f
Gid Baerridge <gid.berridge@gmail.com>
2022-10-27 09:39:42 +0100

    all

--- my_site/settings.py
+++ my_site/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from  os import getenv
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +27,9 @@ SECRET_KEY = 'django-insecure-4m!i!-qb$dp$o9@8)u=@^d)cbkz^a0$wvjey&uds6b2q(*$(1c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    getenv("APP_HOST")
+]
 
 
 # Application definition
