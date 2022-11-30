eff98b36d933b5b051d97926f8e41320ad6b64b2
Gid <gid@Gids-iMac.local>
2022-11-30 11:26:17 +0000

    all

--- my_site/settings.py
+++ my_site/settings.py
@@ -87,15 +87,15 @@ WSGI_APPLICATION = 'my_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("NAME"),
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': os.environ.get("NAME"),
         'USER': os.environ.get("USER"),
         'PASSWORD': os.environ.get("PASSWORD"),
         'HOST': os.environ.get("HOST"),
         'PORT': os.environ.get("PORT"),
 
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME':  BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME':  BASE_DIR / 'db.sqlite3',
     }
 
 }
